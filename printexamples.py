255a19ed72e851bbc620a21f8ef3a51c76c341eb
Almudena <almuyork789@gmail.com>
2023-01-26 10:59:45 +0100

    this is the third session class example

---
+++ printexamples.py
@@ -0,0 +1,7 @@
+print ('this is a test', end='|I want this at the end|\n')
+#instead of having an ENTER separation between the two lines of code, you have what is written in END
+#n adds an ENTER so that there is a space between the lines of code
+print ('this is another')
+print ('this', 2+2, 'be', 'written', 'like', 'this')
+#this is a not so useful comment
+print ('this', 2+2, 'be', 'written', 'like', 'this', sep='!b ?!')
